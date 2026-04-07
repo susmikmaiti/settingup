600f0aef7c2e9df1f8aad8435e2a81c6e5517093
susmikmaiti <susmik787@gmail.com>
2026-04-08 01:07:37 +0530

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM node:22
+
+WORKDIR /app
+
+COPY package*.json ./
+
+RUN npm install
+
+COPY . .
+
+ENV PORT = 9000
+
+EXPOSE 9000
+
+CMD ["node","index.js"]
---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+const port = 9000;
+
+app.get("/",(req,res)=>{
+    res.send(`<h1>Hello, does this work?</h1>`);
+})
+
+app.listen(port,()=>{
+    console.log(`Shit running on http://localhost:${port}`);
+})
